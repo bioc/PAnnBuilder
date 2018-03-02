495de5f56408cfe398cb7beb54b3bf1b73e66c53
lshep <shepherl@gmail.com>
2018-03-02 10:05:23 -0500

    Package deprecation

--- R/zzz.R
+++ R/zzz.R
@@ -10,3 +10,9 @@
     options(PAnnBuilderSourceUrls=tmpEnv$sourceURLs)
     rm(tmpEnv)
 }
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.7")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
